c603b7d1afcde642fdc5526056af35b772e17509
camel-97 <cfmcmillan@icloud.com>
2025-12-30 20:09:49 +0000

    returning data or null condition

--- src/productfetcher.js
+++ src/productfetcher.js
@@ -1,14 +1,18 @@
 export default async function productFetcher() {
     try {
-        const response = await fetch('https://fakestoreapi.com/products/10')
+        const response = await fetch('https://fakestoreapi.com/products')
 
         if (!response.ok) {
             throw new Error(`HTTP errr! status: ${response.status}`)
         }
 
         const productData = await response.json();
+
+        return productData;
     }
     catch (error) {
         console.error('❌ Failed to fetch product:', error.message)
+        return null;
     }
+    
 }
