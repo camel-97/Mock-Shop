dc2f1dcbd8b2d537f58a68fe2f62b60ed2217cf4
camel-97 <cfmcmillan@icloud.com>
2025-12-31 09:05:33 +0000

    updated css imports

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { RouterProvider, createBrowserRouter } from 'react-router'
 
-import './index.css'
+import './css/index.css'
 
 import Layout from './layout/Layout'
 
