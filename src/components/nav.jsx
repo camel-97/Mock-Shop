5d585fcac2b0ad16ab77c6bcae02b28cfe4009b2
camel-97 <cfmcmillan@icloud.com>
2026-01-01 11:44:30 +0000

    cart drilled to nav, cart quantity reduced, conditional render of count by cart in navbar

--- src/components/nav.jsx
+++ src/components/nav.jsx
@@ -1,7 +1,10 @@
 import { Link } from "react-router";
 import '../css/nav.css'
 
-function Nav() {
+function Nav({cart}) {
+
+    const totalqty = cart.reduce((total, item) => total + item.quantity, 0);
+
     return (
         <div className="nav-bar">
             <div className="logo">Mock-Shop</div>
@@ -9,7 +12,11 @@ function Nav() {
                 <Link to="/">Home</Link>
                 <Link to="/shop">Shop</Link>
                 <div>
-                    <Link to="/cart">Cart</Link>                  
+                    <Link to="/cart">Cart</Link>
+                    {totalqty === 0 ? (null) : (
+                    <div className="cart-icon">
+                        {totalqty}
+                    </div>)}                  
                 </div>
             </div>
         </div>
