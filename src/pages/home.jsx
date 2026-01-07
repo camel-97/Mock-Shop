2f078fffcdd45bcaa5a0e2b79c237c1deb6040e6
camel-97 <cfmcmillan@icloud.com>
2026-01-07 08:58:16 +0000

    mounting use effectto cycle thru background images, mapping images and adding class periodically

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,7 +1,33 @@
+import { useEffect, useState } from 'react'
+import image1 from '../assets/shalom-ejiofor-kVUAsl-elOY-unsplash.jpg'
+import image2 from '../assets/shalom-ejiofor-t_prchAm4ag-unsplash.jpg'
+import image3 from '../assets/shalom-ejiofor-YLSBNapbXXY-unsplash.jpg'
 
 function Home() {
-    return(
-        <p>this would be the homepage section</p>
+
+    const images = [image1, image2, image3]
+
+    const [current, setCurrent] = useState(0);
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setCurrent(prev => (prev + 1) % images.length);
+        }, 5000);
+
+        return () => clearInterval(interval)
+    }, [])
+
+
+    return (
+        <div className='carousel'>
+            {images.map((src, index) => (
+                <img
+                    key={index}
+                    src={src}
+                    className={index === current ? 'active' : 'inactive'}
+                />
+            ))}
+        </div>
     )
 }
 
