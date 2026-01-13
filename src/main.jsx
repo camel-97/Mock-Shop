b6e0736fed62069d46bb59e723e5b27233f7ecbc
camel-97 <cfmcmillan@icloud.com>
2026-01-13 17:18:03 +0000

    reconfiguring and checking slashes and case type for pushing to pages

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import {
-  HashRouter,
+  HashRouter as Router,
   Routes,
   Route
 } from 'react-router-dom'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <HashRouter>
+    <Router>
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
@@ -16,6 +16,6 @@ createRoot(document.getElementById('root')).render(
           <Route path="cart" element={<Cart />} />
         </Route>
       </Routes>
-    </HashRouter>
+    </Router>
   </StrictMode>,
 )
