ebda1a1d6a74ceae07cb85494a45aa1c700446e3
camel-97 <cfmcmillan@icloud.com>
2026-01-13 16:22:32 +0000

    moved base outside of test

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/Mock-Shop/',
   test: {
     globals: 'true',
-    base: '/Mock-Shop/',
     environment: 'jsdom',
     setupFiles: './src/tests/setup.js'
   },
